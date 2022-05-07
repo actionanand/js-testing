6a8584b8818137af8339cc707636c7045aacb5e5
Anand raja <meetanandns@gmail.com>
2022-05-07 19:53:12 +0000

    more test cases

--- basic-testing-frontend/src/math.spec.js
+++ basic-testing-frontend/src/math.spec.js
@@ -3,6 +3,30 @@ import { it, expect } from 'vitest';
 import { add } from './math';
 
 it('should sumarize all values.', () => {
-  const result = add([1, 3, 4]);
-  expect(result).toBe(8);
+  // Arrange
+  const inputArray = [1, 2, 5];
+  const expectedVal = inputArray.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+  // Act
+  const result = add(inputArray);
+
+  // Assert
+  expect(result).toBe(expectedVal);
+});
+
+it('should yield NaN, if value is string.', () => {
+  const inputArray = ['number1', 3];
+
+  const result = add(inputArray);
+
+  expect(result).toBeNaN();
+});
+
+it('should yield correct sum, if numeric value is as string', () => {
+  const inputArray = ['3', '7'];
+  const expectedVal = inputArray.reduce((prevValue, curValue) => +prevValue + +curValue, 0);
+
+  const result = add(inputArray);
+
+  expect(result).toBe(expectedVal);
 });
