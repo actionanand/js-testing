dd02fc0f0ee755b541a8bb97530fd00e27c25a4e
Anand raja <meetanandns@gmail.com>
2022-05-08 14:52:52 +0000

    more test for number fn

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -8,5 +8,17 @@ it('should return a number, if number in string format is provided.', () => {
 
   const result = transformToNumber(num);
 
+  expect(result).toBeTypeOf('number');
   expect(result).toBe(expectedVal);
+});
+
+it('should yield NaN, if value is non-number string.', () => {
+  const num1 = 'num1';
+  const num2 = {};
+
+  const result1 = transformToNumber(num1);
+  const result2 = transformToNumber(num2);
+
+  expect(result1).toBeNaN();
+  expect(result2).toBeNaN();
 });
