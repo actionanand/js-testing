706e4322a7365d66e8ded3c0914c9dc095c4555f
Anand raja <meetanandns@gmail.com>
2022-05-09 18:18:31 +0000

    vi mock

--- side-effects-and-mocks/src/util/io.spec.js
+++ side-effects-and-mocks/src/util/io.spec.js
@@ -1,11 +1,18 @@
-import { it, expect } from 'vitest';
+import { it, expect, vi } from 'vitest';
+import { promises as fs } from 'fs';
 
 import writeData from './io';
+
+vi.mock('fs');
 
 it('should execute writeFile method.', () => {
   const testData = 'test data.';
   const testFileName = 'text.txt';
 
   // it'll be resolved, but no value will be returned
-  return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+  // return expect(writeData(testData, testFileName)).resolves.toBeUndefined();
+
+  writeData(testData, testFileName);
+
+  expect(fs.writeFile).toBeCalled();
 });
