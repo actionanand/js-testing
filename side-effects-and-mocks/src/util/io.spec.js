3d87d8c694cf148d97ae5e00f72f28fc704f55f0
Anand raja <meetanandns@gmail.com>
2022-05-09 18:50:30 +0000

    customizing mock

--- side-effects-and-mocks/src/util/io.spec.js
+++ side-effects-and-mocks/src/util/io.spec.js
@@ -5,6 +5,17 @@ import writeData from './io';
 
 vi.mock('fs');
 
+// as path is imported as default, we're mocking join from default
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1] // path.join() receives numerous args, but last element will be file name as per our function, 'storagePath' will be filename
+      }
+    }
+  }
+});
+
 it('should execute writeFile method.', () => {
   const testData = 'test data.';
   const testFileName = 'text.txt';
@@ -14,5 +25,6 @@ it('should execute writeFile method.', () => {
 
   writeData(testData, testFileName);
 
-  expect(fs.writeFile).toBeCalled();
+  // expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFileName, testData);
 });
